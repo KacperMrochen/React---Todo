38764974e1b659cc82335ab62444d4363b758545
KacperMrochen <kacper.mrochen@o2.pl>
2022-09-29 13:57:57 +0200

    First steps - creating components

---
+++ src/form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = () => {
+    
+    return (
+        <div className="form">
+            <h2>What is your task?</h2>
+            <input className="textInput"></input>
+            <input type="submit" value="Add"></input>
+        </div>
+    )
+}
+
+export default Form;
---
+++ src/list.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Todos from "./todos";
+const List = () => {
+    return (
+        <ul>
+            <Todos />
+        </ul>
+    )
+}
+
+export default List;
